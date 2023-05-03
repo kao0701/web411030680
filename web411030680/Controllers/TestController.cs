159366298b1920a67ba1e56f1ff59171c0f02e75
高宥榛 <s1103068@gm.pu.edu.tw>
2023-05-03 20:48:34 +0800

    新增內容

---
+++ web411030680/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web411030680.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW2()
+        {
+            return View();
+        }
+    }
+}
